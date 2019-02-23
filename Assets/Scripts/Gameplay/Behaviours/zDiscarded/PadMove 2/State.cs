3607cbbccff5e5abadd9946fa9aeff79e81b5f21
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-02-23 14:52:05 +0100

    Removed old scripts

--- Assets/Scripts/Gameplay/Behaviours/zDiscarded/PadMove 2/State.cs
+++
@@ -1,7 +0,0 @@
-﻿public abstract class State
-{
-    public abstract void Enter(PadLateralMove pad);
-    public abstract State HandleInput();
-    public abstract void Update(float deltaTime);
-    public abstract void Exit();
-}
