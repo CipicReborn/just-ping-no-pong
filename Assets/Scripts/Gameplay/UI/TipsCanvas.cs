6dcc5df6777a3a016d765ca49ae276fc0b870247
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-03-09 17:57:01 +0100

    disabled warning of non initialized value for inspector-injected variable

--- Assets/Scripts/Gameplay/UI/TipsCanvas.cs
+++ Assets/Scripts/Gameplay/UI/TipsCanvas.cs
@@ -3,8 +3,14 @@ using UnityEngine.UI;
 
 public class TipsCanvas : MonoBehaviour
 {
+    #region INJECTION VIA UNITY INSPECTOR
+    #pragma warning disable CS0649 // Disable warning "variable never initialized"
+
     [SerializeField]
     private Button NextButton;
+
+    #pragma warning restore CS0649
+    #endregion
 
     public void OnEnable()
     {
