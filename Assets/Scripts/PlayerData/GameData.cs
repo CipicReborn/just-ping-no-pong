c27f7a68850793d85b0753fa9e6e8d2eea02adcd
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-03-09 14:42:53 +0100

    Added prototype PlayerData

---
+++ Assets/Scripts/PlayerData/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameData", menuName = "Just Ping No Pong/Player Data/New Game Data")]
+public class GameData : ScriptableObject
+{
+    public class HiScore
+    {
+        string name;
+        int score;
+    }
+
+
+
+    public List<HiScore> HiScores = new List<HiScore>();
+
+    public int Level = 1;
+
+
+}
