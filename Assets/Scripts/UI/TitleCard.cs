158b7874181a6a8b3aba6115a11829d8a102c74d
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-03-09 12:58:25 +0100

    Added Title Card scene and components

---
+++ Assets/Scripts/UI/TitleCard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleCard : MonoBehaviour
+{
+    //[SerializeField]
+    //private Button StartButton;
+    
+    public void OnClickOnStart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
