598ea564bd979ce22226556a1eb0b6f7e0cc19e3
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-03-10 23:13:58 +0100

    Added UI Effect script

---
+++ Assets/Scripts/UI/Effect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
+public class Effect : MonoBehaviour
+{
+#region INJECTION VIA UNITY INSPECTOR
+#pragma warning disable CS0649 // Disable warning "variable never initialized"
+#pragma warning disable IDE0044 // Disable recommendation "Add readonly modifier"
+
+    [SerializeField]
+    private string AnimationName;
+
+#pragma warning restore IDE0044
+#pragma warning restore CS0649
+#endregion
+
+
+    private Animator animator;
+    private AudioSource audioSource;
+    private int animHash;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        animHash = Animator.StringToHash(AnimationName);
+    }
+
+    public void Play()
+    {
+        animator.Play(animHash);
+        if (audioSource.clip != null)
+            audioSource.Play();
+    }
+}
