006a33ca6dbd4830ff82b0723a4f3b10709c60c9
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2019-03-17 13:08:15 +0100

    Added 3rd pad

---
+++ Assets/Scripts/Gameplay/Behaviours/Pad/ProRotationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProRotationController : RotationController
+{
+    public override void Init(Pad pad)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override float GetZAngle()
+    {
+        throw new System.NotImplementedException();
+    }
+}
